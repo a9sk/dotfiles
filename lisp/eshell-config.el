237a7a7ea8d18168562625321e6029b5d719fe12
a9sk <920a9sk42f76c765@proton.me>
2025-10-09 19:43:42 +0200

    eshell-congif.el commented out the my-toggle-eshell-bottom function to avoid conflicts with shell

--- lisp/eshell-config.el
+++ lisp/eshell-config.el
@@ -3,38 +3,36 @@
 ;; load paths from paths.el
 (require 'paths)
 
-;; define my-toggle-eshell function for eshell toggle (i had it in vscode)
-(defun my-toggle-eshell-bottom ()
-  "Toggle eshell in a bottom split window with fixed height, starting at Desktop."
-  (interactive)
-  (let* ((bufname "*eshell*")
-         (buf (get-buffer bufname))
-         (dir my-desktop))
-    (if buf
-        (let ((win (get-buffer-window buf)))
-          (if win
-              (delete-window win)
-            (let ((bottom-window (split-window (frame-root-window) -15 'below)))
-              (set-window-buffer bottom-window buf)
-              (select-window bottom-window)
-              (with-current-buffer buf
-                (eshell/cd dir)))))
-      ;; create buffer
-      (let ((bottom-window (split-window (frame-root-window) -15 'below)))
-        (eshell bufname)
-        (set-window-buffer bottom-window bufname)
-        (select-window bottom-window)
-        (with-current-buffer bufname
-          (eshell/cd dir))))))
+;; this code defines a function to toggle eshell at the bottom of the screen, conflicts with shell-config.el
+;; (so i commented it out for now, but might want to reenable it later)
+;;
+;; ;; define my-toggle-eshell function for eshell toggle (i had it in vscode)
+;; (defun my-toggle-eshell-bottom ()
+;;   "Toggle Eshell at bottom, reuse existing window."
+;;   (interactive)
+;;   (let* ((buf (get-buffer-create "*eshell*"))
+;;          (win (get-buffer-window buf)))
+;;     (if win
+;;         (delete-window win)
+;;       (let ((bottom (split-window (frame-root-window) -15 'below)))
+;;         (select-window bottom)
+;;         (switch-to-buffer buf)
+;;         (unless (eq major-mode 'eshell-mode)
+;;           (eshell-mode))
+;;         (eshell/cd my-desktop)
+;;         (eshell-send-input))))) ;;
 
+;; ;; bind C+ò -> my-toggle-eshell-bottom
+;; (global-set-key (kbd "C-ò") 'my-toggle-eshell-bottom)
 
-;; bind C+ò -> my-toggle-eshell-bottom
-(global-set-key (kbd "C-ò") 'my-toggle-eshell-bottom)
+;; enable ANSI colors in eshell
+(add-hook 'eshell-mode-hook 'ansi-color-for-comint-mode-on)
 
 ;; alias `clear` in eshell to eshell/clear (built in) to avoid line duplication
 (defun my-eshell-clear-alias ()
   "Alias 'clear' to 'eshell/clear' in eshell."
   (eshell/alias "clear" "eshell/clear"))
+  
 (add-hook 'eshell-mode-hook 'my-eshell-clear-alias)
 
 ;;;;;;;;;;;;;;;
