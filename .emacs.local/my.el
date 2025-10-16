2bbe3bd93076d3ff49e4e0d048a911ad19c97920
a9sk <emiliano.rizzonelli@proton.me>
2025-10-16 12:59:22 +0200

    my.el now opens the xterm terminal in a more optimal way

--- .emacs.local/my.el
+++ .emacs.local/my.el
@@ -1,7 +1,11 @@
 ;; helper to open same terminal buffer everytime
 (defun my/open-terminal ()
+  "Open or switch to vterm in a bottom split."
   (interactive)
-  (if (get-buffer "*vterm*")
-      (switch-to-buffer "*vterm*")
-    (vterm)))
-
+  (let ((vterm-buffer (get-buffer "*vterm*")))
+    (unless (window-live-p (get-buffer-window vterm-buffer))
+      (split-window-below)
+      (other-window 1))
+    (if vterm-buffer
+        (switch-to-buffer vterm-buffer)
+      (vterm)))) 
