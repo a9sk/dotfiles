d2f79f87129173b3335a7b8f94e21edbf1d164b9
a9sk <emiliano.rizzonelli@proton.me>
2025-10-17 14:05:38 +0200

    [c++-mode] refractored main snippet to use std::endl, and other small tweaks

--- .emacs.rc/ui.el
+++ .emacs.rc/ui.el
@@ -12,7 +12,7 @@
 (defun rc/default-font ()
   (cond
    ((eq system-type 'windows-nt) "consolas-13")
-   ((eq system-type 'gnu/linux) "iosevka-20")))
+   ((eq system-type 'gnu/linux) "iosevka nerd font-15")))
 
 (add-to-list 'default-frame-alist `(font . ,(rc/default-font)))
 
@@ -20,4 +20,7 @@
 (add-to-list 'custom-theme-load-path
              "~/dotfiles/themes/gruber-darker-theme")
 (load-theme 'gruber-darker t)
+
+;; scale the entire frame, including mode-line and minibuffer
+(add-to-list 'default-frame-alist '(font . "iosevka nerd font-17"))
 
