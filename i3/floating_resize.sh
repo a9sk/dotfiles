ce13e5bc6d0e7ba43049fa3acaa55a0a9667f165
a9sk <emiliano.rizzonelli@proton.me>
2025-10-22 13:59:49 +0200

    [i3] contains configuration and a few scripts for i3 window manager

---
+++ i3/floating_resize.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+i3-msg floating toggle
+sleep 0.1
+i3-msg resize set width 98 ppt height 95 ppt
+i3-msg move position center
---
+++ i3/lock.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# lock screen with style
+i3lock \
+  --inside-color=1a1a1acc \
+  --ring-color=888888cc \
+  --line-color=00000000 \
+  --keyhl-color=ccccccaa \
+  --bshl-color=999999aa \
+  --separator-color=00000000 \
+  --insidever-color=1a1a1acc \
+  --insidewrong-color=1a1a1acc \
+  --ringver-color=aaaaaacc \
+  --ringwrong-color=bb3b3bcc \
+  --verif-color=ffffffff \
+  --wrong-color=ffffffff \
+  --greeter-text="" \
+  --indicator \
+  --radius=120 \
+  --blur 5
