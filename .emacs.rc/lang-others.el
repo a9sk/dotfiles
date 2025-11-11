79ec8f1195a1b5677aa25509ba10f06e27aba1b5
a9sk <emiliano.rizzonelli@proton.me>
2025-11-11 19:12:41 +0100

    [emacs] added initial support for rust language

--- .emacs.rc/lang-others.el
+++ .emacs.rc/lang-others.el
@@ -6,3 +6,19 @@
 (use-package yaml-mode
   :ensure t
   :mode ("docker-compose[^/]*\\.ya?ml\\'" . yaml-mode))
+
+;; Rust language config
+(use-package rust-mode
+  :ensure t
+  :mode ("\\.rs\\'" . rust-mode)
+  :hook (rust-mode . lsp-deferred)
+  :config
+  (setq rust-format-on-save t))
+
+(use-package cargo)
+
+;; TOML language config
+(use-package toml-mode)
+
+;; JSON language config
+(use-package json-mode)
