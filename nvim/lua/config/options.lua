136a00154dbc6136326d670b0e54f441d6756e4f
a9sk <emiliano.rizzonelli@proton.me>
2025-11-18 23:39:25 +0100

    [neovim] initial nvim config dotfiles, basic lazy-nvim and few other things

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+-- NOTE: relative number is nice if i want to move with Xk/Xj...
+-- vim.opt.relativenumber = true
+
+vim.opt.shiftwidth = 4
+
+vim.o.clipboard = "unnamedplus"
+
+
+
+
+
